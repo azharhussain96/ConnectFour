1bf6b5fa0318b3f7b4b5bb4a38dda54d341471ca
alecobban <alecobban@gmail.com>
2016-03-06 16:21:59 -0500

    Added Comments
    Added comments and minor fixes to ConnectFourMove and ConnectFourGame
    classes

--- src/ConnectFourGame.java
+++ src/ConnectFourGame.java
@@ -1,45 +1,88 @@
 /**
- * Created by azharhussain on 2/27/16.
+ * ConnectFourGame
+ * This class stores the information for the state of the connect four game and provides
+ * various methods of interaction with the game
+ * @author Alec Cobban
+ * @author Azhar Hussain
+ * Last edited 3/6/16
  */
 public class ConnectFourGame implements Connect4State {
-
+		// a 2d array of chars represents the board
     private char [][] board = new char[ROWS][COLS];
+    //An array of two players that represents the two players
     private Player [] players;
+    //the number of the player to move, initializes at 0
     private int playerToMoveNum = 0;
+    //passes in the type of view being used to make the game
     private Connect4View gameView;
-
+    
+    /**
+     * This is the constructor for a game of ConnectFour.
+     * sets up various instance variables as well as the board
+     * @param playerNum Which player goes first
+     * @param player a list of players to be fed in
+     * @param view the view that this object will be using to communicate with the user
+     */
     public ConnectFourGame(int playerNum, Player[] player, Connect4View view){
+    		//the number of the player to move is set
         playerToMoveNum = playerNum;
+        //the player list is initialized
         players = player;
+        //the view is fed into the class
         gameView = view;
-
+        
+        //the board is created
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
                 board[i][j] = '.';
             }
         }
     }
-
+    
+    /**
+     * Getter method for the board
+     * @return char[][] the board
+     */
     public char[][] getBoard() {
         return board;
     }
-
+    
+    /**
+     * Getter method for the players
+     * @return Player[] a list of the players for this game
+     */
     public Player[] getPlayers() {
         return players;
     }
-
+    
+    /**
+     * Getter method for the player number to determine whose turn it is
+     * @return int the current player's number
+     */
     public int getPlayerNum() {
         return playerToMoveNum;
     }
-
+    
+    /**
+     * Getter method for the player to move
+     * @return player returns the player whose turn it is
+     */
     public Player getPlayerToMove(){
         return players[playerToMoveNum];
     }
-
+    
+    /**
+     * Setter method to edit the board
+     * @param board the board to set the instance variable to
+     */
     public void setBoard(char[][] board) {
         this.board = board;
     }
-
+    
+    /**
+     * Returns true if move is within range, otherwise returns false.
+     * @return a boolean to determine whether the move is valid
+     */
     public boolean isValidMove(int col){
         if (col > 5 || col < 0){
             return false;
@@ -52,37 +95,61 @@ public class ConnectFourGame implements Connect4State {
         }
         return false;
     }
-
+    
+    /**
+     * Places a piece on the board in the desired column
+     * @param the column that you wish to place the piece in
+     */
     public void makeMove(int col){
+    		//Go through all the rows for the given column
         for (int i = 0; i < ROWS; i++){
+        		// if that spot is empty
             if (board[i][col] == (EMPTY)){
-                if (playerToMoveNum == 0){
+                
+            		//if the first player
+            		if (playerToMoveNum == 0){
+            				//put player ones checker
                     board[i][col] = CHECKERS[0];
                     break;
                 } else {
+                	//or must be player two so put their checker
                     board[i][col] = CHECKERS[1];
                     break;
                 }
             }
         }
     }
-
+    
+    /**
+     * Returns a boolean that tells whether the entire board is full
+     * @return boolean- is the entire board full
+     */
     public boolean isFull(){
         for (int i = 0; i < ROWS; i++){
             for (int j = 0; j < COLS; j++){
+            		//if any spot is empty return false
                 if (board[i][j] == EMPTY){
                     return false;
                 }
             }
 
         }
-        return true;
+        return true; //else return true
     }
-
+    /**
+     * determine if a spot is empty
+     * @param i the row of the spot
+     * @param j the column of the spot
+     * @return whether or not that spot is empty
+     */
     public boolean notEmpty(int i, int j){
         return board[i][j] != EMPTY;
     }
-
+    
+    /**
+     * Look and see if there is a winner. Look at every spot and see if there are four in a row
+     * @return true if there are four in a row otherwise return false
+     */
     public boolean isWinner(){
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
@@ -99,7 +166,11 @@ public class ConnectFourGame implements Connect4State {
         }
         return false;
     }
-
+    
+    /**
+     * Tells you whether or not the game is over
+     * @return returns true if the board is full or if there's a winner, else returns false
+     */
     public boolean gameIsOver(){
         if (isFull()){
             return true;
@@ -108,11 +179,15 @@ public class ConnectFourGame implements Connect4State {
         else
             return false;
     }
-
+    /**
+     * Switch playerToMoveNum between 0 and 1.
+     */
     public void switchPlayer(){
+    		// if 0 then 1
         if (playerToMoveNum == 0){
             playerToMoveNum = 1;
         } else {
+        		//if one then 0
             playerToMoveNum = 0;
         }
     }
--- src/ConnectFourMove.java
+++ src/ConnectFourMove.java
@@ -1,27 +1,55 @@
 /**
- * Created by azharhussain on 2/28/16.
+ * ConnectFourMove
+ * This is a class that defines a move type with value and position
+ * for purposes of static evaluation.
+ * @author Alec Cobban
+ * @author Azhar Hussain
+ * Last edited 3/6/16
  */
-public class ConnectFourMove {
-    private int value;
-    private int move;
 
+public class ConnectFourMove {
+	//the value of the move from a static eval
+    private int value; 
+  //the location of a move (column
+    private int move; 
+
+    /**
+     * Constructor for a ConnectFourMove
+     * assigns the instance variables value and column
+     * @param val the value of the move
+     * @param mov the column of the move
+     */
     public ConnectFourMove(int val, int mov){
-        value = val;
+        //the value of a move
+    		value = val; 
+        //the column that the move is played in
         move = mov;
     }
-
+    /**
+     * Getter function on instance variable value
+     * @return the value of the move
+     */
     public int getValue() {
         return value;
     }
-
+    /**
+     * Getter function on the instance variable move
+     * @return the column that the move was played in
+     */
     public int getMove() {
         return move;
     }
-
+    /**
+     * Setter for value instance variable
+     * @param value the value of the move
+     */
     public void setValue(int value) {
         this.value = value;
     }
-
+    /**
+     * Setter for move instance variable
+     * @param move the column a move is in
+     */
     public void setMove(int move) {
         this.move = move;
     }
